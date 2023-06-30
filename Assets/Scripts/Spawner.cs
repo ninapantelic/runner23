abc948cac7c9b573e8c567beb667d1d3d4935869
prosenicaa <andjelaprosenica@gmail.com>
2023-06-30 17:48:12 +0200

    Azuriran Spawner i dodata skripta Obstacle

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public struct SpawnableObject
     {
-        
+        public GameObject prefab;
+        [Range(0f, 1f)]
+        public float spawnChance;
     }
 
-    // Update is called once per frame
-    void Update()
+    public SpawnableObject[] objects;
+    private System.Random random;
+
+    public float minSpawnRate = 1f;
+    public float maxSpawnRate = 2f;
+
+    private void OnEnable()
     {
-        
+        random = new System.Random();
+        Spawn();
     }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void Spawn()
+    {
+        float spawnChance = (float)random.NextDouble();
+
+        foreach (var obj in objects)
+        {
+            if (spawnChance < obj.spawnChance)
+            {
+                GameObject obstacle = Instantiate(obj.prefab);
+                obstacle.transform.position += transform.position;
+                break;
+            }
+
+            spawnChance -= obj.spawnChance;
+        }
+
+        Invoke(nameof(Spawn), UnityEngine.Random.Range(minSpawnRate, maxSpawnRate));
+    }
+
 }
