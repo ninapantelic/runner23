d67ac999238dd48a2f64512e3d8d8b8a593b8137
prosenicaa <andjelaprosenica@gmail.com>
2023-06-30 17:56:06 +0200

    Azurirani Player i GM sa GameOver

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour
     public float gameSpeedIncrease = 0.1f;
     public float gameSpeed { get; private set; }
 
+    private Player player;
+    private Spawner spawner;
+
 
     private void Awake()
     {
@@ -33,17 +36,36 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-       
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
 
         NewGame();
     }
 
     public void NewGame()
     {
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach (var obstacle in obstacles)
+        {
+            Destroy(obstacle.gameObject);
+        }
+
         gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
     }
 
-    
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+    }
 
     private void Update()
     {
